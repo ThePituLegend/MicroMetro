007224c58779b82688b3b644c4611de7cd385405
ThePituLegend <oscar.lostes@e-campus.uab.cat>
2020-08-31 18:04:46 +0200

    Mas cosas del viejo al nuevo

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    [Range(0f, 1f)][SerializeField] static float popUpFactor = 0.5f;
+    void OnMouseEnter(){
+        transform.localScale *= (1 + popUpFactor);
+    }
+    
+    void OnMouseExit(){
+        transform.localScale /= (1 + popUpFactor);
+    }
+}
